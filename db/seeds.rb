5f1a801e3b8a6d2c78cbde8c8a0aeaaa59475fc0
Atul Bhosale <atul@joshsoftware.com>
2015-05-09 16:36:19 +0530

    Added migrations and seed file.

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Company.create(name: 'Facebook')
+Company.create(name: 'Twitter')
+Company.create(name: 'Amazon')
+Company.create(name: 'Flipkart')
+Individual.create(name: 'Mark')
+Individual.create(name: 'David')
+Individual.create(name: 'Peter')
+Individual.create(name: 'Rob')
